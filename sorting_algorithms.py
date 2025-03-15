92128fd17bc264380bf3cd743775a9a8ca62b7f1
MK27MK <mciartist@gmail.com>
2025-03-15 07:30:15 -0400

    code formatted with black and added a few comments in merge_sort

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -16,6 +16,7 @@ def merge(left, right, values):
 
     i_left = i_right = j = 0
 
+    # comparing left and right sublists and merging them in values
     while i_left < len(left) and i_right < len(right):
         if left[i_left] <= right[i_right]:
             values[j] = left[i_left]
@@ -57,44 +58,53 @@ def merge_sort(values):
 
     return values
 
+
+# ---------------------------------------------------------------------#
+# Quick sort
+# ---------------------------------------------------------------------#
+
+
 def quick_sort(values, start, end):
-        """
-        The Quick sort algorithm is a divide and impera algorithm that use a
-        pivot and move elements bigger than pivot to the right and  smaller than pivot to the left
-        then recursively repeat the procedure for left and right sublists.
-        Time complexity: O(n log n)
-        """
+    """
+    The Quick sort algorithm is a divide and impera algorithm that use a
+    pivot and move elements bigger than pivot to the right and  smaller than pivot to the left
+    then recursively repeat the procedure for left and right sublists.
+    Time complexity: O(n log n)
+    """
     if start < end:
-        q= partition(values, start, end)
-        quick_sort(values, start, q-1)
-        quick_sort(values, q+1, end)
+        q = partition(values, start, end)
+        quick_sort(values, start, q - 1)
+        quick_sort(values, q + 1, end)
 
 
-def partition(values,start, end):
+def partition(values, start, end):
     """
     partion helper function that swap values and return the i'th
     index from where to start and where to end the next sublists division.
     """
 
-    pivot=values[end]
-    i = start -1
+    pivot = values[end]
+    i = start - 1
 
     for j in range(start, end):
         if values[j] <= pivot:
-            i+=1
-            values[i], values[j]=values[j], values[i]
-    #print(i+1)
-    values[i+1], values[end] = values[end], values[i+1]
-    return i+1
+            i += 1
+            values[i], values[j] = values[j], values[i]
+    # print(i+1)
+    values[i + 1], values[end] = values[end], values[i + 1]
+    return i + 1
 
 
+# ---------------------------------------------------------------------#
+# Insertion sort
+# ---------------------------------------------------------------------#
 
-
+...
 
 if __name__ == "__main__":
     test_size = 160
     values = [randint(0, 100) for _ in range(test_size)]
     print("Before sorting: ", *values)
-    #values = merge_sort(values)
-    #quick_sort(values, 0, len(values)-1)
+    # values = merge_sort(values)
+    # quick_sort(values, 0, len(values)-1)
     print("After sorting: ", values)
