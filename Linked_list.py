d3d18d97ade9ae754146d14a16364eff436ce6d7
AliceSantoro01 <detectingdemiurge@gmail.com>
2025-03-31 16:09:11 +0200

    fixes

--- Linked_list.py
+++ Linked_list.py
@@ -57,79 +57,33 @@ class LinkedList:
 
     
     def successor(self, value):
-        current = self.head
+        aus = self.head
         succ = None
-        while current:
-            if current.value > value:
-                if succ is None or current.value < succ.value:
-                    succ = current
-            current = current.next
+        while aus:
+            if aus.value > value:
+                if succ is None or aus.value < succ.value:
+                    succ = aus
+            aus = aus.next
         return succ
     
 
 
     def predecessor(self, value):
-        current = self.head
+        aus = self.head
         prec = None
-        while current:
-            if current.value < value:
-                if prec is None or current.value > prec:
-                    prec = current.value
-            current = current.next
+        while aus:
+            if aus.value < value:
+                if prec is None or aus.value > prec:
+                    prec = aus.value
+            aus = aus.next
         return prec
     
     
     
     def search(self, value):
-        current = self.head
-        while current:
-            if current.value == value:
-                return current  
-            current = current.next
+        aus = self.head
+        while aus:
+            if aus.value == value:
+                return aus
+            aus = aus.next
         return None  
-
-
-
-ll = LinkedList()
-ll.insert_at_head(5)
-ll.insert_at_head(10)
-ll.insert_at_head(3)
-ll.insert_at_head(7)
-print("Ll:", ll)  
-
-
-node = ll.search(10)
-print("Node with value:", node)
-print("Minimum value:", ll.minimum())  
-print("Maximum value:", ll.maximum())  
-
-
-succ_node = ll.successor(5)
-print("Successor:", succ_node)
-
-prec_value = ll.predecessor(5)
-print("Predecessor of 5:", prec_value)  
-
-
-
-ll2 = LinkedList()
-ll2.insert_at_head("pippo")
-ll2.insert_at_head("paperino")
-ll2.insert_at_head("pluto")
-ll2.insert_at_head("clarabella")
-
-print ("LL2:",ll2)
-print("Pippo's successor:",ll2.successor("pippo"))
-print("Pippo's predecessor:",ll2.predecessor("pippo"))
-print("Clarabella's predecessor:",ll2.predecessor("clarabella"))
-print ("Minimum:", ll2.minimum())
-
-
-ll3 = LinkedList()
-ll3.insert_at_head(4.1)
-ll3.insert_at_head(2.3)
-ll3.insert_at_head(5.7)
-ll3.insert_at_head(8.2)
-ll3.insert_at_head(-5.2)
-print ("ll3:",ll3)
-print("minimum:",ll3.minimum())
